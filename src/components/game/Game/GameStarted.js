424191538818315acc20599d0ce2c1467183b3ff
Tobias Lundgren <tobias@lundgren.design>
2019-04-04 12:55:41 +0200

    refactor: separate components

--- src/components/game/Game/GameStarted.js
+++ src/components/game/Game/GameStarted.js
@@ -6,27 +6,32 @@ import { UPDATE_STORY_SUBSCRIPTION } from '../queries';
 export default class GameStarted extends Component {
   render() {
     const { id, title, votes } = this.props.story;
-    return (
-      <Subscription
-        subscription={UPDATE_STORY_SUBSCRIPTION}
-        // variables={story.id}
-      >
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>;
-          if (error) return <p>Error :</p>;
-          console.log(data);
-          return (
-            <>
-              <H2>Game Started</H2>
-              <h3>Votes</h3>
-              {JSON.stringify(data, null, 0)}
-              {/* {votes.map(data.vote => (
-                <div>{data.story.vote.value}</div>
-              ))} */}
-            </>
-          );
-        }}
-      </Subscription>
-    );
+    return <LiveView />;
   }
 }
+
+export const LiveView = () => (
+  <Subscription
+    subscription={UPDATE_STORY_SUBSCRIPTION}
+    // variables={story.id}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :</p>;
+      console.log(data);
+
+      const { id, title, votes } = data.storyUpdated;
+
+      return (
+        <>
+          <H2>Game Started</H2>
+          <h3>Votes</h3>
+          {votes.map(vote => (
+            <div>{vote.value}</div>
+          ))}
+          {JSON.stringify(data, null, 0)}
+        </>
+      );
+    }}
+  </Subscription>
+);
